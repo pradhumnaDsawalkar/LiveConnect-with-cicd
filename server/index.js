b6a10f58af615ab10c4a6066a82ce3c2a1e353b3
pradhumnaDsawalkar <pradhumna0007@gmail.com>
2025-07-04 23:29:32 +0530

    First

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const axios = require("axios");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
